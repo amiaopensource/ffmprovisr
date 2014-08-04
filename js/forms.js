13e897fca1689fc2f4d720d1bab51bb77b96c93b
ablwr <ashley.blewer@gmail.com>
2014-08-03 22:14:14 -0400

    abstracted form away for future form options

--- js/forms.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  
-  $('#generate').click(function(){
-    $('#command_line').val("ffmpeg" + 
-      " -i " + $('#input_name').val() + 
-      " -o " + $('#output_name').val())
-  });
-
-});
