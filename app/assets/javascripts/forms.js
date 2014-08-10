87a9c814326f1d9a4feae8046de859bce4484004
ablwr <ashley.blewer@gmail.com>
2014-08-10 13:21:58 -0400

    committing before switching

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,14 +1,38 @@
 $(document).ready(function() {
-  
+
+  $( "#show_display_info" ).click(function() {
+    $( ".display_info" ).toggle();
+  });
+
+  $( "#show_web" ).click(function() {
+    $( ".web" ).toggle();
+  });
+
+  $( "#show_add_subtitles" ).click(function() {
+    $( ".add_subtitles" ).toggle();
+  });
+
   $('#generate').click(function(){
     $('#command_line').val("ffmpeg" + 
       " -i " + $('#input_name').val() + 
-      " -o " + $('#output_name').val())
+      " " + $('#output_name').val())
   });
 
   $('#display_generate').click(function(){
     $('#display_command_line').val("ffmpeg" + 
       " -i " + $('#input_name').val())
   });
+
+  $('#web_generate').click(function(){
+    $('#web_command_line').val("ffmpeg" + 
+      " -i " + $('#input_name').val())
+  });
+
+  $('#add_subtitles_generate').click(function(){
+    $('#command_line').val("ffmpeg" + 
+      " -i " + $('#input_name').val() + 
+      " " + $('#output_name').val())
+  });
+
 
 });
