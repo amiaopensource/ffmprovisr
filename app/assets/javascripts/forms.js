e8faa05bb80e19a45361c30de1487c0ac86bb383
ablwr <ashley.blewer@gmail.com>
2014-08-12 21:21:24 -0400

    a few functioning

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -12,6 +12,14 @@ $(document).ready(function() {
     $( ".add_subtitles" ).toggle();
   });
 
+  $( "#show_add_audio_track" ).click(function() {
+    $( ".add_audio_track" ).toggle();
+  });
+
+  $( "#show_remove_audio_track" ).click(function() {
+    $( ".remove_audio_track" ).toggle();
+  });
+
   $('#generate').click(function(){
     $('#command_line').val("ffmpeg" + 
       " -i " + $('#input_name').val() + 
@@ -29,10 +37,18 @@ $(document).ready(function() {
   });
 
   $('#add_subtitles_generate').click(function(){
-    $('#command_line').val("ffmpeg" + 
-      " -i " + $('#input_name').val() + 
-      " " + $('#output_name').val())
+    $('#subtitles_command_line').val("ffmpeg" + 
+      " -i " + $('#subtitles_input_name').val() + " -vf subtitles=" + $('#subtitles_name').val() + " " + $('#subtitles_output_name').val())
   });
 
+  $('#add_audio_track_generate').click(function(){
+    $('#add_audio_track_command_line').val("ffmpeg" + 
+      " -i " + $('#audio_input_name').val() + " -i " + $('#audio_track_name').val() + " " + $('#audio_output_name').val())
+  });
+
+  $('#remove_audio_track_generate').click(function(){
+    $('#remove_audio_track_command_line').val("ffmpeg" + 
+      " -i " + $('#r_audio_input_name').val() + " " + $('#r_audio_track_name').val() + " -vcodec copy -an " + $('#r_audio_output_name').val())
+  });
 
 });
