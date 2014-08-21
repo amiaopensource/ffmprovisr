6d34f6506142d985f30381a0430b63eb2afedb13
ablwr <ashley.blewer@gmail.com>
2014-08-21 10:46:09 -0400

    h264 for web command added

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -29,6 +29,10 @@ $(document).ready(function() {
     $( ".webm_for_web" ).toggle();
   });
 
+  $( "#show_h264_for_web" ).click(function() {
+    $( ".h264_for_web" ).toggle();
+  });
+
 
   $('#generate').click(function(){
     $('#command_line').val("ffmpeg" + 
@@ -67,6 +71,14 @@ $(document).ready(function() {
       " -codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10 -qmax 42 -maxrate 500k -bufsize 1000k -threads 4 -vf scale=-1:480 -codec:a libvorbis -b:a 128k " + 
       $('#webm_output_name').val())
   });
+
+  $('#h264_for_web_generate').click(function(){
+    $('#h264_for_web_command_line').val("ffmpeg" + 
+      " -i " + $('#h264_input_name').val() + 
+      " -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 -codec:a libfdk_aac -b:a 128k " + 
+      $('#h264_output_name').val())
+  });
+
 
 });
 
