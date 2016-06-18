3f42736f98016e0641de02c85a76f8cfddd74f30
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-06-18 22:44:36 +0100

    js.js - copy @helrond commit for crals
    https://github.com/dd388/crals/commit/7c614298789dbda49b3637c79c2ad0b6f0924519

--- js/js.js
+++ js/js.js
@@ -1,3 +1,28 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(document).ready(function() {
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+  // open modal window if a hash is found in URL
+  if(window.location.hash) {
+    $(window.location.hash).modal('show');
+    // add direct link to modal window
+    $(".link").empty();
+    $(".link").append("<small>Link to this command: "+window.location.href+"</small>");
+  }
+
+  // add hash to URL when modal is opened
+  $('span[data-toggle="modal"]').on("click", function(){
+    window.location.hash = $(this).attr("data-target");
+    // add direct link to modal window
+    $(".link").empty();
+    $(".link").append("<small>Link to this command: "+window.location.href+"</small>");
+  });
+
+  // remove hash from URL when modal is closed
+  $(document).on('hide.bs.modal', function (e) {
+    history.pushState("", document.title, window.location.pathname);
+  });
+
+});
