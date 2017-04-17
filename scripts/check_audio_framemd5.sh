e762c7dc427e01f83f053e1cfa69da4b64477e0c
Reto Kromer <retokromer@users.noreply.github.com>
2017-04-17 07:57:49 +0200

    plural

--- scripts/check_audio_framemd5.sh
+++ scripts/check_audio_framemd5.sh
@@ -32,7 +32,7 @@ Dependency:
   ffmpeg
 About:
   Version: ${VERSION}
-  Website: https://github.com/amiaopensource/ffmprovisr/blob/gh-pages/script/check_audio_framemd5.sh
+  Website: https://github.com/amiaopensource/ffmprovisr/blob/gh-pages/scripts/check_audio_framemd5.sh
 EOF
     exit 0
 }
