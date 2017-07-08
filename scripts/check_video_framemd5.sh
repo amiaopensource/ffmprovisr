7fe503d3245f96135523b3cfdd25b86407173aaf
Reto Kromer <retokromer@users.noreply.github.com>
2017-07-08 12:48:15 +0200

    uniform

--- scripts/check_video_framemd5.sh
+++ scripts/check_video_framemd5.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
   SCRIPT=$(basename "${0}")
- VERSION='2017-04-17'
+ VERSION='2017-07-08'
   AUTHOR='ffmprovisr'
      RED='\033[1;31m'
     BLUE='\033[1;34m'
       NC='\033[0m'
 
-if [[ ${OSTYPE} = "cygwin" ]] || [ ! $(which diff) ]; then
+if [[ "${OSTYPE}" = "cygwin" ]] || [ ! $(which diff) ]; then
     echo -e "${RED}Error: 'diff' is not installed by default. Please install 'diffutils' from Cygwin.${NC}"
     exit 1
 fi
 
 _output_prompt(){
     cat <<EOF
-Usage: ${SCRIPT} -h | -i <av_file> -m <md5_file>
+Usage: ${SCRIPT} -i <av_file> -m <md5_file> | -h
 EOF
     exit 1
 }
@@ -21,13 +21,11 @@ EOF
 _output_help(){
     cat <<EOF
 Syntax:
-  ${SCRIPT}
-    Prompts a short help message.
-  ${SCRIPT} -h
-    This help.
   ${SCRIPT} -i <av_file> -m <md5_file>
     Pass to the script the audio-visual file and the corresponding MD5
     file to check.
+  ${SCRIPT} -h
+    This help.
 Dependency:
   ffmpeg
 About:
@@ -40,11 +38,11 @@ EOF
 unset input_file
 unset input_hash
 
-while getopts ":hi:m:" opt; do
+while getopts ":i:m:h" opt; do
     case "${opt}" in
+        i) input_file=${OPTARG} ;;
+        m) input_hash=${OPTARG} ;;
         h) _output_help ;;
-        i) input_file=$OPTARG ;;
-        m) input_hash=$OPTARG ;;
         :) echo -e "${RED}Error: option -${OPTARG} requires an argument${NC}" ; _output_prompt ;;
         *) echo -e "${RED}Error: bad option -${OPTARG}${NC}" ; _output_prompt ;;
     esac
@@ -53,7 +51,7 @@ done
 [[ -z "${#}" || ! ${input_file} || ! ${input_hash} ]] && _output_prompt
 echo -e "${BLUE}Please wait...${NC}"
 unset md5_tmp
-if [[ $OSTYPE = "cygwin" ]]; then
+if [[ "${OSTYPE}" = "cygwin" ]]; then
     md5_tmp="${USERPROFILE}/$(basename "${input_hash}").tmp"
 else
     md5_tmp="${HOME}/$(basename "${input_hash}").tmp"
