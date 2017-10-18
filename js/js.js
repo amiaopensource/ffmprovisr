c18a193973221c2e7e1fca3c85942d4d97c279b7
Ashley Blewer <ashley.blewer@gmail.com>
2017-10-18 17:17:55 -0400

    patches link to command javascript

--- js/js.js
+++ js/js.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
 
   // open recipe window if a hash is found in URL
   if(window.location.hash) {
-    id = window.location.hash.substring(1)
-    document.getElementById(id).checked = true;
-    $('html, body').animate({ scrollTop: $(window.location.hash).offset().top}, 1000);
-    $(id + " .link").empty();
-    $(id + " .link").append("<small>Link to this command: <a href="+window.location.href+">"+window.location.href+"</a></small>");
+    id = window.location.hash
+    document.getElementById(id.substring(1)).checked = true;
+    $('html, body').animate({ scrollTop: $(id).offset().top}, 1000);
+    $(id).closest('div').find('.link').empty();
+    $(id).closest('div').find('.link').append("<small>Link to this command: <a href="+window.location.href+">"+window.location.href+"</a></small>");
   }
 
   // add hash URL when recipe is opened
   $('label[class="recipe"]').on("click", function(){
     id = $(this).attr("for");
     window.location.hash = id
-    $(id + " .link").empty();
-    $(id + " .link").append("<small>Link to this command: <a href="+window.location.href+">"+window.location.href+"</a></small>");
+    $(id).closest('div').find('.link').empty();
+    $(id).closest('div').find('.link').append("<small>Link to this command: <a href="+window.location.href+">"+window.location.href+"</a></small>");
   });
 
 });
