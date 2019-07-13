07fe8bf9665c2a81ad34d305100af3c15ce0e498
Ashley Blewer <ashley.blewer@gmail.com>
2019-07-13 18:41:44 -0400

    Move repeated JS into functions

--- js/js.js
+++ js/js.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
 
-  // open recipe window if a hash is found in URL
-  if(window.location.hash) {
-    id = window.location.hash
-    document.getElementById(id.substring(1)).checked = true;
-    $('html, body').animate({ scrollTop: $(id).offset().top}, 1000);
+  function appendLink(id) {
+    console.log(id)
     $(id).closest('div').find('.link').empty();
-    $(id).closest('div').find('.link').append("<small>Link to this command: <a href='https://amiaopensource.github.io/ffmprovisr/index.html"+window.location.hash+"'>https://amiaopensource.github.io/ffmprovisr/index.html"+window.location.hash+"</a></small>");
+    $(id).closest('div').find('.link')
+      .append("<small>Link to this command: <a href='https://amiaopensource.github.io/ffmprovisr/index.html" + id + "'>https://amiaopensource.github.io/ffmprovisr/index.html" + id + "</a></small>");
+
+  }
+
+  function moveToRecipe(id) {
+    document.getElementById(id.substring(1)).checked = true;
+    $('html, body').animate({ scrollTop: $(id).offset().top }, 1000);
+    appendLink(id)
+  }
+
+  // open recipe window if a hash is found in URL
+  if (window.location.hash) {
+    id = window.location.hash
+    moveToRecipe(id)
   }
 
   // add hash URL when recipe is opened
   $('label[class="recipe"]').on("click", function(){
     id = $(this).attr("for");
     window.location.hash = ('#' + id)
-    $('#' + id).closest('div').find('.link').empty();
-    $('#' + id).closest('div').find('.link').append("<small>Link to this command: <a href='https://amiaopensource.github.io/ffmprovisr/index.html"+window.location.hash+"'>https://amiaopensource.github.io/ffmprovisr/index.html"+window.location.hash+"</a></small>");
-  });
+    appendLink('#' + id)
+  })
 
   // open recipe when clicked
   $('a').on("click", function(){
     intralink = $(this).attr("href")
     if (intralink[0] == "#") {
-      document.getElementById(intralink.substring(1)).checked = true;
-      $('html, body').animate({ scrollTop: $(intralink).offset().top }, 1000);
+      moveToRecipe(intralink)
     }
   })
 
